0a6a7b7f15dcf0513ba8f6cbd78f668b955b9269
Jay Sprout <jotasprout@gmail.com>
2023-09-29 13:53:44 +0200

    First bit of fun with personal app blog

---
+++ blog-react/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./comps/Home";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Personal Project #5,348</h1>
+        <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ blog-react/src/comps/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div>
+            <img src='https://www.roxorsoxor.com/build/imgs/argyleGuitars2.jpg' alt='Warlock electric guitars filled with an argyle pattern' />       
+        </div>
+
+    );
+}
