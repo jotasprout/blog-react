52900355e8000d195f1bc962c420cfc7bc59e22d
Jay Sprout <jotasprout@gmail.com>
2023-12-20 12:33:07 -0500

    Building port

--- blog-react/src/comps/portfolio/PortList.js
+++ blog-react/src/comps/portfolio/PortList.js
@@ -1,7 +1,22 @@
+// import { Link } from "react-router-dom";
+// import { useEffect, useState } from "react";
+import PortItem from "./PortItem";
 export default function PortList(){
+
+    let storyline = [];
+    let camtasia = [];
+    let print = [];
+    let jobHelpers = [];
+    let video = [];
+    let gamification = [];
+    let flash = [];
+    let webDev = [];
+
     return(
         <div>
             <p>Not here. You're looking for my portfolio.</p>
+            <PortItem />
         </div>
     );
+
 }
